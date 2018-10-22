36a0cf1face0b79589e19d8a5da2aef63b33f2de
yamachan435 <cyama435@gmail.com>
2018-10-22 23:39:49 +0900

    Login inplemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   resources :book_infos
   resources :books
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   root 'static_pages#index'
   get '/rental', to: 'static_pages#rental'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_053819) do
+ActiveRecord::Schema.define(version: 2018_10_22_124632) do
 
   create_table "book_infos", force: :cascade do |t|
     t.string "isbn"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2018_10_15_053819) do
     t.binary "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
